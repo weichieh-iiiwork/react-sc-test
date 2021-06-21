76c1ed7200667f3dc9c4d0d9e9975640a09b4262
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-06-21 20:55:40 +0800

    0621 想用for迴圈 但失敗

--- src/components/StepRow.js
+++ src/components/StepRow.js
@@ -3,10 +3,37 @@ import React from 'react'
 function StepRow(props) {
     const { scStep } = props
 
+    // const scStepText = (step) =>{
+    //     for(let i=0; i<step.len; i++){
+    //         if(i === step.len){
+    //             return (
+    //             <div className="step">
+    //             <div className="d-flex align-items-center">
+    //             <i className="stepCircle stepActive"></i>
+    //             </div>
+    //             <div className="stepFont">{step[i]}</div>
+    //             </div>
+    //             )
+    //         }
+    //         return (
+    //         <div className="step">
+    //             <div className="d-flex align-items-center">
+    //                 <i className="stepCircle stepActive"></i>
+    //                 <i className="stepLine"></i>
+    //                 <i className="stepArrow right"></i>
+    //             </div>
+    //             <div className="stepFont">{step[i]}</div>
+    //         </div>
+    //         )
+    //     }
+    // }
+    
+
   return(
     <>
       {/* <!-- 購物車流程 --> */}
       <div className="col-10 px-0 d-flex mx-auto justify-content-center">
+        {/* {scStepText(scStep)} */}
           <div className="step">
             <div className="d-flex align-items-center">
               <i className="stepCircle stepActive"></i>
